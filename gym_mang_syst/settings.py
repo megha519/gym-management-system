58512bd578b037d51263e3f0e5026bcf5d956101
megha519 <122044071+megha519@users.noreply.github.com>
2026-06-17 21:36:49 +0530

    Change database configuration to use SQLite
    Switched database backend from PostgreSQL to SQLite.

--- gym_mang_syst/settings.py
+++ gym_mang_syst/settings.py
@@ -82,15 +82,10 @@ ASGI_APPLICATION = 'gym_mang_syst.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER':'postgres',
-        'PASSWORD':'qwert123',
-        'HOST':'127.0.0.1',
-        'PORT':5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
